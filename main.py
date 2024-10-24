cccb9a8bb1afcf27b898bc1a004df72e18731f57
Вадим Голов <grafit-design@inbox.ru>
2024-10-25 00:46:32 +0500

    Написал программу тестирую

--- main.py
+++ main.py
@@ -27,7 +27,39 @@ def warning_box(message_text):
     infobox.showwarning(message=message_text)
     console.destroy()
 
+def upper_level():
 
+    err_upper: bool = False
+
+    try:
+        browser.get('https://ru.wikipedia.org/wiki/Заглавная_страница')
+    except WebDriverException:
+        err_upper: bool = True
+        warning_box('Похоже отсутствует Сеть')
+
+    # Проверяем по заголовку, если нет выходим из программы
+    if "Википедия" not in browser.title:
+        if not err_upper:
+            warning_box('Что-то пошло не так: Я не смог открыть википедию')
+        browser.quit()
+
+    print('\n+---------------------------------------------------------------------+')
+    print('| Похоже там больше не было связанных страниц.                        |')
+    print('| Давайте опять поищем что-нибудь интересное                          |')
+    print('+---------------------------------------------------------------------+\n')
+
+    while True:
+
+        new_request: str = input('Что еще Вы хотите узнать? ')
+        new_search: browser = browser.find_element(By.ID, "searchInput")
+
+        if user_request != '':
+            new_search.send_keys(user_request)
+            new_search.send_keys(Keys.RETURN)
+            option_one()
+
+
+# При выборе опции 1
 def option_one():
 
     print('\n+---------------------------------------------------------------------+')
@@ -35,6 +67,7 @@ def option_one():
     print('+---------------------------------------------------------------------+')
     print('| 1. Листать страницы текущей статьи;                                 |')
     print('| 2. Перейти на одну из связанных страниц.                            |')
+    print('| 3. Завершить работу.                                                |')
     print('+---------------------------------------------------------------------+\n')
 
     sel_request: str = input('Выберите (1/2): ')
@@ -62,7 +95,11 @@ def option_one():
     elif sel_request == '2':
         option_two()
 
+    else:
+        exit()
 
+
+# При выборе опции 2
 def option_two():
 
     print()
@@ -74,11 +111,13 @@ def option_two():
     for div_tag in browser.find_elements(By.TAG_NAME, 'div'):
         # Ищем атрибут класса
         class_attr = div_tag.get_attribute('class')
-        if class_attr == 'hatnote navigation-not-searchable':
+        if class_attr == 'mw-search-result-heading':
             rel_pages.append(div_tag)
 
-    if len(rel_pages) > 1:
-        # Выбор случайной страницы
+    # Выбор случайной страницы
+    if len(rel_pages) == 0:
+        upper_level()
+    elif len(rel_pages) > 1:
         random_page = choice(rel_pages)
     else:
         random_page = rel_pages[0]
@@ -89,10 +128,8 @@ def option_two():
 
     option_one()
 
-# - листать параграфы статьи;
-# - перейти на одну из внутренних статей.
 
-
+# Основная программа
 err_state: bool = False
 browser: webdriver = webdriver.Chrome()
 
